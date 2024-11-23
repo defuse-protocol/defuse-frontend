143dfaa9fa61554c58ee53a6fd67e6608b075f30
Old Emil <cawabunga@defuse.org>
2024-11-23 09:01:05 +0100

    fix: update footer copy

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="w-full flex justify-center items-center py-7">
       <div className="flex justify-center items-center text-sm text-secondary gap-1.5 bg-white px-3 py-1.5 rounded-full dark:bg-gray-700 dark:text-white">
-        <span>Powered by</span>
+        <span>Built by</span>
         <Image
           src="/static/icons/network/near_dark.svg"
           width={16}
@@ -12,6 +12,13 @@ const Footer = () => {
           alt="Near logo"
         />
         <span className="text-black-400 dark:text-white">Near</span>
+        <span>with ❤ to</span>
+        <Image
+          src="/static/logos/SolanaSolLogoHorizontal.svg"
+          width={88}
+          height={22}
+          alt="Solana logo"
+        />
       </div>
     </footer>
   )
