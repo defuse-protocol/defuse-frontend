a3fb3ede3c2a341807d38193efc1c568873a2676
Old Emil <cawabunga@defuse.org>
2025-02-17 10:43:34 +0100

    style: add extra empty line

--- src/types/database-generated.ts
+++ src/types/database-generated.ts
@@ -170,3 +170,4 @@ export type CompositeTypes<
   : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
     ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
     : never
+
