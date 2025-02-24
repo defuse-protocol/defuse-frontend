4660e1e376c0345c4531724fcece6709b442d074
Old Emil <cawabunga@defuse.org>
2025-02-24 22:55:51 +0100

    feat: provide sendNearTransaction function to widgets

--- src/app/otc-desk/create-order/page.tsx
+++ src/app/otc-desk/create-order/page.tsx
@@ -8,6 +8,7 @@ import { createOTCOrderLink } from "@src/app/otc-desk/_utils/link"
 import Paper from "@src/components/Paper"
 import { LIST_TOKENS } from "@src/constants/tokens"
 import { useConnectWallet } from "@src/hooks/useConnectWallet"
+import { useNearWalletActions } from "@src/hooks/useNearWalletActions"
 import { useTokenList } from "@src/hooks/useTokenList"
 import { useWalletAgnosticSignMessage } from "@src/hooks/useWalletAgnosticSignMessage"
 
@@ -16,6 +17,7 @@ export default function CreateOrderPage() {
   const tokenList = useTokenList(LIST_TOKENS)
   const signMessage = useWalletAgnosticSignMessage()
   const { tokenIn, tokenOut } = useDeterminePair()
+  const { signAndSendTransactions } = useNearWalletActions()
 
   return (
     <Paper>
@@ -24,6 +26,20 @@ export default function CreateOrderPage() {
         userAddress={state.isVerified ? state.address : undefined}
         userChainType={state.chainType}
         signMessage={signMessage}
+        sendNearTransaction={async (tx) => {
+          const result = await signAndSendTransactions({ transactions: [tx] })
+
+          if (typeof result === "string") {
+            return { txHash: result }
+          }
+
+          const outcome = result[0]
+          if (!outcome) {
+            throw new Error("No outcome")
+          }
+
+          return { txHash: outcome.transaction.hash }
+        }}
         generateLink={(multiPayload) => {
           console.log("multiPayload", multiPayload)
           return createOTCOrderLink(multiPayload)
