fbb8e3fb74aa0565ddc90d27c0c2a154d4327caa
jobotics <jo_botics@proton.me>
2025-03-17 21:59:43 +0000

    chore: update gift paper container

--- src/app/gift-card/create-gift/page.tsx
+++ src/app/gift-card/create-gift/page.tsx
@@ -1,13 +1,13 @@
 "use client"
 
-import { GiftMakerWidget } from "@defuse-protocol/defuse-sdk"
+import { GiftHistoryWidget, GiftMakerWidget } from "@defuse-protocol/defuse-sdk"
 import { useDeterminePair } from "@src/app/(home)/_utils/useDeterminePair"
 import Paper from "@src/components/Paper"
 import { useConnectWallet } from "@src/hooks/useConnectWallet"
 import { useIntentsReferral } from "@src/hooks/useIntentsReferral"
 import { useTokenList } from "@src/hooks/useTokenList"
 import { useWalletAgnosticSignMessage } from "@src/hooks/useWalletAgnosticSignMessage"
-import React, { useRef } from "react"
+import React from "react"
 import { createGiftCardLink } from "../_utils/link"
 import { safeTokenList } from "../_utils/safeTokenList"
 
@@ -17,11 +17,10 @@ export default function CreateOrderPage() {
   const signMessage = useWalletAgnosticSignMessage()
   const { tokenIn } = useDeterminePair()
   const referral = useIntentsReferral()
-  const portalRef = useRef<HTMLDivElement | null>(null)
 
   return (
     <Paper>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center gap-8">
         <GiftMakerWidget
           tokenList={tokenList}
           userAddress={state.isVerified ? state.address : undefined}
@@ -33,9 +32,12 @@ export default function CreateOrderPage() {
             return createGiftCardLink(secretKey)
           }}
           initialToken={tokenIn}
-          // portalRef={portalRef}
         />
-        <div ref={portalRef} className="w-full mt-8" />
+        {/* <GiftHistoryWidget
+          userAddress={state.isVerified ? state.address : undefined}
+          userChainType={state.chainType}
+          generateLink={(secretKey) => createGiftCardLink(secretKey)}
+        /> */}
       </div>
     </Paper>
   )
