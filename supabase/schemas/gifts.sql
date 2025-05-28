d72718f9e91788cc15b01bee064b54a0d725a6a2
jobotics <jo_botics@proton.me>
2025-05-28 02:42:58 +0100

    feat: add new gifts table

---
+++ supabase/schemas/gifts.sql
@@ -0,0 +1,13 @@
+CREATE TABLE otc_trades (
+    gift_id TEXT PRIMARY KEY,
+    encrypted_payload TEXT NOT NULL,
+    p_key TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create an update trigger for updated_at
+CREATE TRIGGER otc_gifts_set_updated_at
+    BEFORE UPDATE ON gifts
+    FOR EACH ROW
+    EXECUTE FUNCTION trigger_set_timestamp();
