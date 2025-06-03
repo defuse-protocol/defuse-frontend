b095d202f3608da327ceb1442384ccb77d3f73f8
Old Emil <cawabunga@defuse.org>
2025-06-03 18:53:05 +0200

    feat: add dummy tonconnect manifest

--- src/providers/TonConnectUIProvider.tsx
+++ src/providers/TonConnectUIProvider.tsx
@@ -1,11 +1,24 @@
 "use client"
 
+import { NODE_IS_DEVELOPMENT } from "@src/utils/environment"
 import { TonConnectUIProvider } from "@tonconnect/ui-react"
 import type { ReactNode } from "react"
 
 function TonConnectUIProviderWrapper({ children }: { children: ReactNode }) {
   return (
-    <TonConnectUIProvider manifestUrl="https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json">
+    <TonConnectUIProvider
+      manifestUrl={
+        NODE_IS_DEVELOPMENT
+          ? // TON Keeper extension does not load manifest from http://localhost, so we fallback to the demo app
+            "https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json"
+          : `${window.location.origin}/tonconnect-manifest.json`
+      }
+      walletsRequiredFeatures={
+        {
+          // signData: { types: ["text"] }, // This will be needed, but it does not work
+        }
+      }
+    >
       {children}
     </TonConnectUIProvider>
   )
