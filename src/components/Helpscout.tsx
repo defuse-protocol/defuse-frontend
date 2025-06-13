341ee03f45509d72011dc506aa589f799878d4a8
jobotics <jo_botics@proton.me>
2025-06-13 14:50:10 +0100

    fix: move env variable to all environment files

--- src/components/Helpscout.tsx
+++ src/components/Helpscout.tsx
@@ -1,3 +1,4 @@
+import { HELPSCOUT_BEACON_ID } from "@src/utils/environment"
 import Script from "next/script"
 
 const Helpscout = () => {
@@ -7,7 +8,7 @@ const Helpscout = () => {
         {`!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});`}
       </Script>
       <Script id="helpscout-beacon-init" strategy="lazyOnload">
-        {`window.Beacon('init', '${process.env.NEXT_PUBLIC_HELPSCOUT_BEACON_ID}')`}
+        {`window.Beacon('init', '${HELPSCOUT_BEACON_ID}')`}
       </Script>
     </>
   )
