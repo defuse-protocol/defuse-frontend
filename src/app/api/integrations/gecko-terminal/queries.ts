0ffd3b3ceaba617f1e5bbf3851eed2417edf8bea
defuse-es <es@defuse.org>
2025-06-13 16:30:17 +0200

    improve performance

--- src/app/api/integrations/gecko-terminal/queries.ts
+++ src/app/api/integrations/gecko-terminal/queries.ts
@@ -1,6 +1,13 @@
 import { PAIR_SEPARATOR } from "./utils"
 
 export const EVENTS_QUERY = `
+WITH distinct_assets as (
+  SELECT
+    DISTINCT defuse_asset_id,
+    decimals
+  FROM
+    near_intents_db.defuse_assets
+)
 SELECT
   CAST(any(d.block_height) AS UInt32) AS blockNumber,
   toUnixTimestamp(any(d.block_timestamp)) AS blockTimestamp,
@@ -12,16 +19,28 @@ SELECT
     '${PAIR_SEPARATOR}',
     argMax(d.token_out, d.token_out IS NOT NULL)
   ) AS pairId,
-  printf('%.0f', sumIf(abs(d.amount_in), d.amount_in IS NOT NULL)) AS asset0In,
-  printf('%.0f', sumIf(d.amount_out, d.amount_out IS NOT NULL)) AS asset1Out,
-  printf('%.0f', sumIf(abs(d.amount_in), d.amount_in IS NOT NULL)) AS reserveAsset0,
-  printf('%.0f', sumIf(d.amount_out, d.amount_out IS NOT NULL)) AS reserveAsset1,
+  printf(
+    '%.0f',
+    sumIf(abs(d.amount_in), d.amount_in IS NOT NULL)
+  ) AS asset0In,
+  printf(
+    '%.0f',
+    sumIf(d.amount_out, d.amount_out IS NOT NULL)
+  ) AS asset1Out,
+  printf(
+    '%.0f',
+    sumIf(abs(d.amount_in), d.amount_in IS NOT NULL)
+  ) AS reserveAsset0,
+  printf(
+    '%.0f',
+    sumIf(d.amount_out, d.amount_out IS NOT NULL)
+  ) AS reserveAsset1,
   argMax(asset_in.decimals, d.token_in IS NOT NULL) AS asset0Decimals,
   argMax(asset_out.decimals, d.token_out IS NOT NULL) AS asset1Decimals
 FROM
   near_intents_db.silver_dip4_token_diff_new d
-  LEFT JOIN near_intents_db.defuse_assets asset_in ON d.token_in = asset_in.defuse_asset_id
-  LEFT JOIN near_intents_db.defuse_assets asset_out ON d.token_out = asset_out.defuse_asset_id
+  LEFT JOIN distinct_assets asset_in ON d.token_in = asset_in.defuse_asset_id
+  LEFT JOIN distinct_assets asset_out ON d.token_out = asset_out.defuse_asset_id
 WHERE
   d.tokens_cnt = 2
   AND d.tx_hash IS NOT NULL
@@ -35,7 +54,4 @@ HAVING
   count(DISTINCT d.token_out) = 1
   AND count(DISTINCT d.token_in) = 1
   AND asset0Decimals != 0
-  AND asset1Decimals != 0
-ORDER BY
-  blockNumber ASC,
-  eventIndex ASC`
+  AND asset1Decimals != 0`
