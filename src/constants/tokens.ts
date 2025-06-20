432cde00d0de8c81110cbfd422cd44f7b24f778b
Old Emil <cawabunga@defuse.org>
2025-06-20 20:21:11 +0200

    Allow deposit base usdc from near

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -39,6 +39,17 @@ export const LIST_TOKENS: TokenWithTags[] = [
       },
       {
         defuseAssetId:
+          "nep141:base-0x833589fcd6edb6e08f4c7c32d4f71b54bda02913.omft.near",
+        address: "base-0x833589fcd6edb6e08f4c7c32d4f71b54bda02913.omft.near",
+        decimals: 6,
+        icon: "https://s2.coinmarketcap.com/static/img/coins/128x128/3408.png",
+        chainName: "near",
+        bridge: "direct",
+        symbol: "USDC",
+        name: "USD Coin",
+      },
+      {
+        defuseAssetId:
           "nep141:17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1",
         address: "0x368ebb46aca6b8d0787c96b2b20bd3cc3f2c45f7",
         decimals: 6,
