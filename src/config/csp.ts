d90078e888cbb5c37e838cb41a1beca3dfebc731
jobotics <jo_botics@proton.me>
2025-07-08 14:20:53 +0100

    chore: update connect-src directive in CSP

--- src/config/csp.ts
+++ src/config/csp.ts
@@ -106,6 +106,7 @@ const cspConfig = {
     "https://ton.api.onfinality.io",
     "https://ton.api.onfinality.io/public",
     "https://toncenter.com/api/v2/jsonRPC",
+    "https://horizon.stellar.org",
   ],
   "object-src": ["'none'"],
   "base-uri": ["'self'"],
