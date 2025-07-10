6f4da3618961ab8e821513e30b805c6c763842f7
hyper54n3 <hypersane1984@proton.me>
2025-07-10 18:38:43 +0100

    fix: remove tokenIn and tokenOut logic (separate PR)

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,6 @@
 "use client"
 import { SwapWidget } from "@defuse-protocol/defuse-sdk"
-import { useSearchParams } from "next/navigation"
-import { useMemo } from "react"
 
-import { useDeterminePair } from "@src/app/(home)/_utils/useDeterminePair"
 import Paper from "@src/components/Paper"
 import { LIST_TOKENS } from "@src/constants/tokens"
 import { useConnectWallet } from "@src/hooks/useConnectWallet"
@@ -11,7 +8,6 @@ import { useIntentsReferral } from "@src/hooks/useIntentsReferral"
 import { useNearWalletActions } from "@src/hooks/useNearWalletActions"
 import { useTokenList } from "@src/hooks/useTokenList"
 import { useWalletAgnosticSignMessage } from "@src/hooks/useWalletAgnosticSignMessage"
-import { findTokenBySymbol } from "@src/utils/findTokenBySymbol"
 import { renderAppLink } from "@src/utils/renderAppLink"
 
 export default function Swap() {
@@ -19,20 +15,7 @@ export default function Swap() {
   const signMessage = useWalletAgnosticSignMessage()
   const { signAndSendTransactions } = useNearWalletActions()
   const tokenList = useTokenList(LIST_TOKENS)
-  const { tokenIn: defaultTokenIn, tokenOut: defaultTokenOut } =
-    useDeterminePair()
   const referral = useIntentsReferral()
-  const searchParams = useSearchParams()
-  const tokenInSymbol = searchParams.get("tokenIn")
-  const tokenOutSymbol = searchParams.get("tokenOut")
-
-  const tokenIn = useMemo(() => {
-    return findTokenBySymbol(tokenInSymbol, defaultTokenIn)
-  }, [tokenInSymbol, defaultTokenIn])
-
-  const tokenOut = useMemo(() => {
-    return findTokenBySymbol(tokenOutSymbol, defaultTokenOut)
-  }, [tokenOutSymbol, defaultTokenOut])
 
   return (
     <Paper>
@@ -58,8 +41,6 @@ export default function Swap() {
         renderHostAppLink={renderAppLink}
         userChainType={state.chainType ?? null}
         referral={referral}
-        initialTokenIn={tokenIn}
-        initialTokenOut={tokenOut}
       />
     </Paper>
   )
