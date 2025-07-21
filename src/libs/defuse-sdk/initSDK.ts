daf673acfa4c3c268f4bd8a14d895fbf2f7213b2
jobotics <jo_botics@proton.me>
2025-07-21 20:30:02 +0100

    chore: make Stellar public

--- src/libs/defuse-sdk/initSDK.ts
+++ src/libs/defuse-sdk/initSDK.ts
@@ -10,15 +10,6 @@ export function initSDK() {
   }
   hasInitialized = true
 
-  // TODO: Remove this workaround when Stellar is fully supported.
-  // Note: initSDK may be called both on the server and the client.
-  // On the client, it can be triggered inside a useEffect, so window is accessible.
-  let stellarEnabled = false
-  if (typeof window !== "undefined") {
-    const params = new URLSearchParams(window.location.search)
-    stellarEnabled = params.get("stellar") === "true"
-  }
-
   if (NODE_IS_DEVELOPMENT) {
     configureSDK({
       env: INTENTS_ENV,
@@ -33,8 +24,7 @@ export function initSDK() {
         ton: true,
         near_intents: true,
         sui: true,
-        // TODO: Make it true when Stellar is supported
-        stellar: stellarEnabled,
+        stellar: true,
       },
     })
   } else {
@@ -63,8 +53,7 @@ export function initSDK() {
         ton: true,
         near_intents: true,
         sui: true,
-        // TODO: Make it true when Stellar is supported
-        stellar: stellarEnabled,
+        stellar: true,
       },
     })
   }
