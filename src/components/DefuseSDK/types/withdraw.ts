3582e273e16d8f62127de2c24754a734ebe65e20
jobotics <jo_botics@proton.me>
2025-08-11 13:16:20 +0100

    fix: correct displayAddress type

--- src/components/DefuseSDK/types/withdraw.ts
+++ src/components/DefuseSDK/types/withdraw.ts
@@ -6,7 +6,7 @@ import type { RenderHostAppLink } from "./hostAppLink"
 export type WithdrawWidgetProps = {
   userAddress: authHandle.AuthHandle["identifier"] | undefined
   // Specifically for network like TON, where the userAddress is the public key
-  displayAddress: string | null
+  displayAddress: string | undefined
   chainType: authHandle.AuthHandle["method"] | undefined
   presetTokenSymbol: string | undefined
   presetAmount: string | undefined
