a3942f21b99dc243335a8db94b5fd1ffc74bce13
jobotics <jo_botics@proton.me>
2025-09-02 09:05:21 +0100

    refactor: replace @near-wallet-selector logic with NearWalletProvider

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -7,16 +7,16 @@ import Paper from "@src/components/Paper"
 import { LIST_TOKENS, type TokenWithTags } from "@src/constants/tokens"
 import { useConnectWallet } from "@src/hooks/useConnectWallet"
 import { useIntentsReferral } from "@src/hooks/useIntentsReferral"
-import { useNearWalletActions } from "@src/hooks/useNearWalletActions"
 import { useTokenList } from "@src/hooks/useTokenList"
 import { useWalletAgnosticSignMessage } from "@src/hooks/useWalletAgnosticSignMessage"
+import { useNearWallet } from "@src/providers/NearWalletProvider"
 import { renderAppLink } from "@src/utils/renderAppLink"
 import { useRouter, useSearchParams } from "next/navigation"
 
 export default function Swap() {
   const { state } = useConnectWallet()
   const signMessage = useWalletAgnosticSignMessage()
-  const { signAndSendTransactions } = useNearWalletActions()
+  const { signAndSendTransactions } = useNearWallet()
   const tokenList = useTokenList(filterOutRefAndBrrrTokens(LIST_TOKENS))
   const { tokenIn, tokenOut } = useDeterminePair()
   const referral = useIntentsReferral()
