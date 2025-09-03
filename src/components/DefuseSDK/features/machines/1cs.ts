979826a0144c2d272c462720bbb556a49859484a
defuse-es <es@defuse.org>
2025-09-03 16:06:59 +0200

    use zod

--- src/components/DefuseSDK/features/machines/1cs.ts
+++ src/components/DefuseSDK/features/machines/1cs.ts
@@ -11,18 +11,11 @@ import { computeAppFeeBps } from "@src/components/DefuseSDK/utils/appFee"
 import { LIST_TOKENS } from "@src/constants/tokens"
 import { APP_FEE_BPS, APP_FEE_RECIPIENT } from "@src/utils/environment"
 import { unstable_cache } from "next/cache"
-import * as v from "valibot"
 import z from "zod"
 import { isBaseToken } from "../../utils/token"
 
-OpenAPI.BASE = v.parse(
-  v.pipe(v.string(), v.nonEmpty()),
-  process.env.ONE_CLICK_URL
-)
-OpenAPI.TOKEN = v.parse(
-  v.pipe(v.string(), v.nonEmpty()),
-  process.env.ONE_CLICK_API_KEY
-)
+OpenAPI.BASE = z.string().parse(process.env.ONE_CLICK_URL)
+OpenAPI.TOKEN = z.string().parse(process.env.ONE_CLICK_API_KEY)
 
 export async function getTokens() {
   return await getTokensCached()
