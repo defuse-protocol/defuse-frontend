58ef257704b02a503d7840e75d9d21353ed39368
jobotics <jo_botics@proton.me>
2025-09-08 12:16:40 +0100

    refactor: update environment variables

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,21 +1,14 @@
 import * as v from "valibot"
 
-export const NEAR_NODE_URL =
-  process.env.nearNodeUrl ?? "https://rpc.mainnet.near.org"
-export const NEAR_ENV = process.env.NEAR_ENV ?? "testnet"
+export const APP_ENV = process.env.NEXT_PUBLIC_APP_ENV === "development"
+export const RUNTIME_NODE_JS = process.env.NEXT_RUNTIME === "nodejs"
+export const RUNTIME_EDGE = process.env.NEXT_RUNTIME === "edge"
 
-export const NODE_IS_DEVELOPMENT = process.env.NODE_ENV === "development"
-export const NEXT_RUNTIME_NODE_JS = process.env.NEXT_RUNTIME === "nodejs"
-export const NEXT_RUNTIME_EDGE = process.env.NEXT_RUNTIME === "edge"
-
-export const NEXT_PUBLIC_LINK_DOCS = process.env.NEXT_PUBLIC_LINK_DOCS ?? ""
-export const NEXT_PUBLIC_PUBLIC_MAIL =
-  process?.env?.NEXT_PUBLIC_PUBLIC_MAIL ?? ""
-export const NEXT_PUBLIC_PUBLIC_TG = process?.env?.NEXT_PUBLIC_PUBLIC_TG ?? ""
-
-export const SOCIAL_LINK_X = process?.env?.socialX ?? ""
-export const SOCIAL_LINK_DISCORD = process?.env?.socialDiscord ?? ""
-export const LINK_DOCS = process?.env?.socialDocs ?? ""
+export const MAIL = process?.env?.NEXT_PUBLIC_MAIL ?? ""
+export const LINK_DOCS = process.env.NEXT_PUBLIC_LINK_DOCS ?? ""
+export const LINK_TG = process?.env?.NEXT_PUBLIC_LINK_TG ?? ""
+export const LINK_X = process?.env?.NEXT_PUBLIC_LINK_X ?? ""
+export const LINK_DISCORD = process?.env?.NEXT_PUBLIC_LINK_DISCORD ?? ""
 
 export const VERCEL_PROJECT_PRODUCTION_URL = process.env
   .VERCEL_PROJECT_PRODUCTION_URL
@@ -25,14 +18,8 @@ export const VERCEL_PROJECT_PRODUCTION_URL = process.env
 export const PROJECT_ID = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? ""
 
-export const APP_URL = process?.env?.appUrl ?? "/"
-
 export const SUPABASE_URL = process.env.SUPABASE_URL
 export const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY
-export const SOLANA_RPC_URL = process.env.SOLANA_RPC_URL
-
-export const DEV_MODE = process?.env?.NEXT_PUBLIC_DEV_MODE === "true"
-export const TURN_OFF_APPS = process?.env?.turnOffApps === "true"
 
 export const INTENTS_ENV = v.parse(
   v.picklist(["production", "stage"]),
