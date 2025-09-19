915fdada29e4c05f1925dd547c4125b1a0e45e6f
jobotics <jo_botics@proton.me>
2025-09-19 11:14:59 +0100

    feat: add ClickHouse client

---
+++ src/utils/clickhouse.ts
@@ -0,0 +1,26 @@
+import { createClient } from "@clickhouse/client"
+import {
+  CLICK_HOUSE_PASSWORD,
+  CLICK_HOUSE_URL,
+  CLICK_HOUSE_USERNAME,
+} from "@src/utils/environment"
+
+export const clickHouseClient = createClient({
+  url: CLICK_HOUSE_URL,
+  username: CLICK_HOUSE_USERNAME,
+  password: CLICK_HOUSE_PASSWORD,
+})
+
+/**
+ * Helper to run a ClickHouse query and return typed rows.
+ */
+export async function chQuery<T>(
+  query: string,
+  query_params?: Record<string, unknown>
+): Promise<T[]> {
+  const { data } = await clickHouseClient
+    .query({ query, ...(query_params ? { query_params } : {}) })
+    .then((res) => res.json<T>())
+
+  return data
+}
