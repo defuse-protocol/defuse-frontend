00bc61c223d06d51be0c5bc9095130b4ab622082
jobotics <jo_botics@proton.me>
2025-09-29 21:56:11 +0100

    chore: make ADA token publicly available (#657)

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -1659,7 +1659,7 @@ const PRODUCTION_TOKENS: TokenInfo[] = [
     bridge: "poa",
     symbol: "ADA",
     name: "ADA",
-    tags: ["mc:10", "feature:ada"],
+    tags: ["mc:10"],
   },
   {
     defuseAssetId: "nep245:v2_1.omni.hot.tg:10_vLAiSt9KfUGKpw5cD3vsSyNYBo7",
