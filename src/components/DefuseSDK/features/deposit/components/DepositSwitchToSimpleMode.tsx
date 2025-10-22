81aee6127ada8bb7997320b4faca60a38338127a
jobotics <jo_botics@proton.me>
2025-10-22 10:20:41 +0100

    feat: add switch for deposit simple mode

---
+++ src/components/DefuseSDK/features/deposit/components/DepositSwitchToSimpleMode.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link"
+import { useSearchParams } from "next/navigation"
+import { useMemo } from "react"
+
+export function DepositSwitchToSimpleMode() {
+  const searchParams = useSearchParams()
+
+  const newSearchParams = useMemo(() => {
+    const newSearchParams = new URLSearchParams(searchParams)
+    newSearchParams.set("not1cs", "true")
+    newSearchParams.delete("1cs")
+    return newSearchParams
+  }, [searchParams])
+
+  return (
+    <div className="text-center mb-5">
+      Try{" "}
+      <Link
+        href={`/deposit?${newSearchParams.toString()}`}
+        className="underline text-blue-c11"
+        prefetch={false}
+      >
+        switching to legacy deposit
+      </Link>{" "}
+      if the problem persists
+    </div>
+  )
+}
