d139e9b803f0f8f45aa36dc4bfb487dbd654fadb
defuse-es <es@defuse.org>
2025-10-23 10:48:07 +0200

    remove on each commit, add env validation

--- test/helpers/constants/pages.ts
+++ test/helpers/constants/pages.ts
@@ -1,3 +1,8 @@
+import { z } from "zod"
+
 export const NEAR_INTENTS_PAGE = {
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  baseURL: z
+    .string()
+    .min(1, "NEXT_PUBLIC_BASE_URL must be a non-empty string")
+    .parse(process.env.NEXT_PUBLIC_BASE_URL),
 }
