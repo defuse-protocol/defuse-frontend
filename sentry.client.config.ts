fd0cfb60624caa7fd13d78a6dc7c51c4d4c48939
Old Emil <cawabunga@defuse.org>
2025-11-04 16:07:27 +0200

    fix: disable sentry traces

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -12,7 +12,7 @@ import { LIST_TOKENS } from "@src/constants/tokens"
 Sentry.init({
   dsn: "https://68e98c6f1b314199f93ab8470623556c@o4510000873668621.ingest.us.sentry.io/4510000882778112",
   enabled: process.env.NEXT_PUBLIC_SENTRY_ENABLED === "true",
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0,
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
   integrations: [
