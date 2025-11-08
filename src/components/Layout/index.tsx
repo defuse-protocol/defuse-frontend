99a09b215eaf253c70067aa8850ba4204582b9fa
jobotics <jo_botics@proton.me>
2025-11-08 22:26:22 +0000

    chore: remove Gnosis outage notifications (#740)

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -6,7 +6,6 @@ import type { PropsWithChildren } from "react"
 import Footer from "@src/components/Layout/Footer"
 import { Header } from "@src/components/Layout/Header"
 import { NavbarMobile } from "@src/components/Navbar/NavbarMobile"
-import NetworkOutageNotification from "@src/components/NetworkOutageNotification"
 import PageBackground from "@src/components/PageBackground"
 import { useMixpanelBus } from "@src/hooks/useMixpanelBus"
 import { usePathLogging } from "@src/hooks/usePathLogging"
@@ -33,7 +32,6 @@ const Layout: React.FC<PropsWithChildren> = ({ children }) => {
           </Header.DepositSlot>
         }
       />
-      <NetworkOutageNotification />
       <Main>{children}</Main>
       <Footer />
       <NavbarMobile />
