7ae0294c0702e8b4f034934131d80043407796b8
jobotics <jo_botics@proton.me>
2025-11-12 10:25:35 +0000

    feat: support validation for Zcash unified addresses (#746)

--- src/components/DefuseSDK/features/withdraw/components/WithdrawForm/components/RecipientSubForm/validationRecipientAddress.test.ts
+++ src/components/DefuseSDK/features/withdraw/components/WithdrawForm/components/RecipientSubForm/validationRecipientAddress.test.ts
@@ -278,6 +278,40 @@ describe("validationRecipientAddress", () => {
   })
 
   describe("Zcash network", () => {
+    const validTransparentAddress = "t1Q879cLgqaCd7zKRi79wQYuGBenmNX6cKn"
+    const validUAAddress =
+      "u1rgqfsvuxkwv7vc54nxd4g733xwh686ur06usz8vucns3ywx350fmeaz5w7a7mxg5u6gp28pufwlmsenxzqhphcl9nt56u5428c836u7wyecxs7alnms08txxr3g4gj850eclnhsfcw2cfll92r3xfd7ydhhpslymygl9qxz4wudrtlfh"
+    const invalidTransparentAddress = "t1r2VHBwC5eAnZB22YNFSJg8iFtWgoAEKW000"
+    const invalidUA = "u1r2VHBwC5eAnZB22YNFSJg8iFtWgoAEKW000"
+
+    it("accepts a valid transparent address", async () => {
+      const result = await validationRecipientAddress(
+        validTransparentAddress,
+        "zcash"
+      )
+      expect(result.isOk()).toBe(true)
+    })
+
+    it("accepts a valid UA address", async () => {
+      const result = await validationRecipientAddress(validUAAddress, "zcash")
+      expect(result.isOk()).toBe(true)
+    })
+
+    it("rejects a transparent address with invalid characters", async () => {
+      const result = await validationRecipientAddress(
+        invalidTransparentAddress,
+        "zcash"
+      )
+      expect(result.isErr()).toBe(true)
+      expect(result.unwrapErr()).toBe("ADDRESS_INVALID")
+    })
+
+    it("rejects a ua address with invalid characters", async () => {
+      const result = await validationRecipientAddress(invalidUA, "zcash")
+      expect(result.isErr()).toBe(true)
+      expect(result.unwrapErr()).toBe("ADDRESS_INVALID")
+    })
+
     it("should accept valid Zcash address", async () => {
       const result = await validationRecipientAddress(
         "t1XJD5btQc9qHHzywPS8xhPbs2hbXGXWLFq",
