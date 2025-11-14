392a27a5de2c8d19482f82606e82de6ff4ba7e9d
defuse-es <es@defuse.org>
2025-11-14 19:07:40 +0100

    show slippage only when rate is visible

--- src/components/DefuseSDK/features/swap/components/SwapRateInfo.tsx
+++ src/components/DefuseSDK/features/swap/components/SwapRateInfo.tsx
@@ -46,8 +46,8 @@ export function SwapRateInfo({ tokenIn, tokenOut }: SwapRateInfoProps) {
 
   return (
     <div className="flex flex-col gap-3.5 font-medium text-gray-11 text-xs">
-      <div className="flex flex-wrap justify-between items-center gap-2 text-gray-11">
-        {rateIsReady ? (
+      {rateIsReady && (
+        <div className="flex flex-wrap justify-between items-center gap-2 text-gray-11">
           <button
             type="button"
             onClick={toggleBasePrice}
@@ -69,26 +69,24 @@ export function SwapRateInfo({ tokenIn, tokenOut }: SwapRateInfoProps) {
                   tokensUsdPriceData,
                 })}
           </button>
-        ) : (
-          <div />
-        )}
 
-        <button
-          type="button"
-          onClick={handleOpenSlippageSettings}
-          className="px-3 py-1.5 rounded-md border transition-all bg-gray-1 border-gray-6 hover:bg-gray-3 hover:border-gray-7 text-label font-medium text-xs cursor-pointer active:scale-[0.98] flex items-center gap-1.5"
-        >
-          <span>
-            Max slippage:{" "}
-            {Intl.NumberFormat(undefined, {
-              style: "percent",
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2,
-            }).format(slippageBasisPoints / Number(BASIS_POINTS_DENOMINATOR))}
-          </span>
-          <SlidersHorizontalIcon className="size-3.5" weight="regular" />
-        </button>
-      </div>
+          <button
+            type="button"
+            onClick={handleOpenSlippageSettings}
+            className="px-3 py-1.5 rounded-md border transition-all bg-gray-1 border-gray-6 hover:bg-gray-3 hover:border-gray-7 text-label font-medium text-xs cursor-pointer active:scale-[0.98] flex items-center gap-1.5"
+          >
+            <span>
+              Max slippage:{" "}
+              {Intl.NumberFormat(undefined, {
+                style: "percent",
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              }).format(slippageBasisPoints / Number(BASIS_POINTS_DENOMINATOR))}
+            </span>
+            <SlidersHorizontalIcon className="size-3.5" weight="regular" />
+          </button>
+        </div>
+      )}
     </div>
   )
 }
