040feeab99c1d4f635f6986ed9229ed31842f44c
defused-ape <defused@postal.ist>
2026-01-21 10:11:15 +0100

    Tweaked error messages

--- src/components/DefuseSDK/features/account/components/IntentCreationResult.tsx
+++ src/components/DefuseSDK/features/account/components/IntentCreationResult.tsx
@@ -16,17 +16,17 @@ const IntentCreationResult = ({
   switch (status) {
     case "ERR_USER_DIDNT_SIGN":
       content =
-        "The transaction confirmation was either cancelled or failed. Please try again, or contact support if necessary."
+        "Your transaction confirmation was either cancelled, or failed. If it failed, you can try again, or contact support."
       break
 
     case "ERR_CANNOT_VERIFY_SIGNATURE":
       content =
-        "We couldn’t verify your signature, please try again with another wallet."
+        "We couldn’t verify your authentication. Please try again, and contact support if this continues."
       break
 
     case "ERR_SIGNED_DIFFERENT_ACCOUNT":
       content =
-        "The message was signed with a different wallet. Please try again."
+        "We couldn’t verify that authentication. Please double-check that you’re not signing with the wrong wallet, and try again, or contact support if this continues."
       break
 
     case "ERR_PUBKEY_ADDING_DECLINED":
@@ -35,29 +35,30 @@ const IntentCreationResult = ({
 
     case "ERR_PUBKEY_CHECK_FAILED":
       content =
-        "We couldn’t verify your key, possibly due to a connection issue."
+        "We couldn’t verify your ’public key’, a tech problem likely caused by a network issue. Please try again, and contact support if this continues."
       break
 
     case "ERR_PUBKEY_ADDING_FAILED":
-      content = "Transaction for adding public key is failed. Please try again."
+      content =
+        "We couldn’t add your ’public key’, a tech problem likely caused by a network issue. Please try again, and contact support if this continues."
       break
 
     case "ERR_PUBKEY_EXCEPTION":
-      content = "An error occurred while adding public key. Please try again."
+      content =
+        "We couldn’t add your ’public key’, a tech problem likely caused by a network issue. Please try again, and contact support if this continues."
       break
 
     case "ERR_QUOTE_EXPIRED_RETURN_IS_LOWER":
-      content =
-        "The quote has expired or the return is lower than expected. Please try again."
+      content = "Your quote unfortunately expired. Please try again."
       break
 
     case "ERR_CANNOT_PUBLISH_INTENT":
       content =
-        "We couldn’t send your request, possibly due to a network issue or server downtime. Please check your connection or try again later."
+        "We couldn’t process your request, likely caused by a network issue. Please try again, and contact support if this continues."
       break
 
     case "ERR_WALLET_POPUP_BLOCKED":
-      content = "Please allow popups and try again."
+      content = "Please enable pop-up windows in your browser, and try again."
       break
 
     case "ERR_WALLET_CANCEL_ACTION":
@@ -73,17 +74,18 @@ const IntentCreationResult = ({
       break
 
     case "ERR_TRANSFER_MESSAGE_FAILED":
-      content = "Failed to create transfer message. Please try again."
+      content =
+        "We were unable to create a ’transfer message’, a tech problem on our side. Please try again, and contact support if this continues."
       break
 
     case "ERR_AMOUNT_IN_BALANCE_INSUFFICIENT_AFTER_NEW_1CS_QUOTE":
       content =
-        "Swap aborted: Insufficient token balance for the updated quote. Please try again."
+        "While processing your swap, the price quote updated to an amount that exceeds the balance in your account. Pleaes try again, and contact support if this continues."
       break
 
     default:
       status satisfies never
-      content = `An error occurred. Please try again. ${status}`
+      content = `An error occurred. Please try again, and contact support and include this status data: ${status}`
   }
 
   if (content == null) {
