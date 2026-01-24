455822a965643a51978bbafe5d62f24859f81685
santerisarle <santeri.sarle@gmail.com>
2026-01-24 09:35:05 +0300

    Improve modal

--- src/features/webauthn/components/WebAuthnDialog.tsx
+++ src/features/webauthn/components/WebAuthnDialog.tsx
@@ -5,6 +5,7 @@ import { BaseModalDialog } from "@src/components/DefuseSDK/components/Modal/Moda
 import { useWebAuthnUIStore } from "@src/features/webauthn/hooks/useWebAuthnUiStore"
 import { PasskeyIcon } from "@src/icons"
 import clsx from "clsx"
+import { useState } from "react"
 import { useForm } from "react-hook-form"
 
 interface FormData {
@@ -12,6 +13,7 @@ interface FormData {
 }
 
 export function WebAuthnDialog() {
+  const [isCreating, setIsCreating] = useState(false)
   const webauthnUI = useWebAuthnUIStore()
   const {
     register,
@@ -20,87 +22,87 @@ export function WebAuthnDialog() {
   } = useForm<FormData>()
 
   return (
-    <BaseModalDialog open={webauthnUI.isOpen} onClose={webauthnUI.close}>
+    <BaseModalDialog
+      open={webauthnUI.isOpen}
+      onClose={webauthnUI.close}
+      back={isCreating ? () => setIsCreating(false) : undefined}
+    >
       <div className="flex flex-col items-center justify-center">
         <div className="size-14 rounded-full bg-gray-100 flex items-center justify-center">
           <PasskeyIcon className="size-6" />
         </div>
-        <h2 className="mt-3 text-xl font-bold text-center text-gray-900 tracking-tight">
-          Sign in with passkey
+        <h2 className="mt-4! text-xl font-bold text-center text-gray-900 tracking-tight">
+          {isCreating ? "Create new passkey" : "Sign in with passkey"}
         </h2>
       </div>
-      <div className="mt-6">
-        <Button
-          onClick={() => webauthnUI.signIn()}
-          loading={webauthnUI.isSigningIn}
-          size="xl"
-          fullWidth
+
+      {isCreating ? (
+        <form
+          onSubmit={handleSubmit(({ passkeyName }) => {
+            void webauthnUI.createNew(passkeyName)
+          })}
+          className="mt-6 space-y-2"
         >
-          {webauthnUI.isSigningIn ? "Connecting..." : "Use existing passkey"}
-        </Button>
+          <label htmlFor="passkeyName" className="sr-only">
+            Passkey label (only visible to you)
+          </label>
+          <input
+            id="passkeyName"
+            type="text"
+            placeholder="Passkey label (only visible to you)"
+            className={clsx(
+              "block rounded-2xl bg-white px-3.5 py-4.5 w-full text-gray-900 font-semibold placeholder:text-gray-400 text-sm leading-none ring-0 border-none outline-1 -outline-offset-1 focus-visible:outline-2 focus-visible:-outline-offset-2",
+              errors.passkeyName
+                ? "outline-red-500 focus-within:outline-red-500"
+                : "outline-gray-200 focus-within:outline-gray-900"
+            )}
+            {...register("passkeyName", { required: true })}
+          />
 
-        {webauthnUI.signInError != null && (
-          <Alert variant="error" className="mt-2">
-            {webauthnUI.signInError}
-          </Alert>
-        )}
-      </div>
+          <Button
+            type="submit"
+            size="xl"
+            fullWidth
+            loading={webauthnUI.isCreating}
+          >
+            {webauthnUI.isCreating ? "Creating..." : "Create new passkey"}
+          </Button>
 
-      <div className="mt-8 flex items-center gap-x-6 self-stretch">
-        <div className="w-full flex-1 border-t border-gray-200" />
-        <p className="text-xs font-semibold text-nowrap text-gray-500 uppercase">
-          Or
-        </p>
-        <div className="w-full flex-1 border-t border-gray-200" />
-      </div>
-
-      <h2 className="mt-8 text-xl font-bold text-center text-gray-900 tracking-tight">
-        Create new passkey
-      </h2>
-
-      <form
-        className="mt-4"
-        onSubmit={handleSubmit(({ passkeyName }) => {
-          void webauthnUI.createNew(passkeyName)
-        })}
-      >
-        <label htmlFor="passkeyName" className="sr-only">
-          Passkey label (only visible to you)
-        </label>
-        <input
-          id="passkeyName"
-          type="text"
-          placeholder="Passkey label (only visible to you)"
-          className={clsx(
-            "block rounded-2xl bg-white px-3 py-4 w-full text-gray-900 font-semibold placeholder:text-gray-400 text-sm leading-none ring-0 border-none outline-1 -outline-offset-1 focus-visible:outline-2 focus-visible:-outline-offset-2",
-            errors.passkeyName
-              ? "outline-red-500 focus-within:outline-red-500"
-              : "outline-gray-200 focus-within:outline-gray-900"
+          {webauthnUI.createError != null && (
+            <Alert variant="error">{webauthnUI.createError}</Alert>
           )}
-          {...register("passkeyName", { required: true })}
-        />
 
-        <Button
-          type="submit"
-          size="xl"
-          fullWidth
-          loading={webauthnUI.isCreating}
-          className="mt-2"
-        >
-          {webauthnUI.isCreating ? "Creating..." : "Create new passkey"}
-        </Button>
-
-        {webauthnUI.createError != null && (
-          <Alert variant="error" className="mt-2">
-            {webauthnUI.createError}
+          <Alert variant="info" className="mt-6">
+            Store your passkeys securely. Losing your passkey means losing
+            access to your account and any associated funds permanently.
           </Alert>
-        )}
-      </form>
+        </form>
+      ) : (
+        <div className="mt-6 space-y-2">
+          <Button
+            onClick={() => webauthnUI.signIn()}
+            loading={webauthnUI.isSigningIn}
+            size="xl"
+            fullWidth
+          >
+            {webauthnUI.isSigningIn ? "Connecting..." : "Use existing passkey"}
+          </Button>
 
-      <Alert variant="info" className="mt-6">
-        Store your passkeys securely. Losing your passkey means losing access to
-        your account and any associated funds permanently.
-      </Alert>
+          <Button
+            onClick={() => setIsCreating(true)}
+            disabled={webauthnUI.isSigningIn}
+            size="xl"
+            fullWidth
+            variant="secondary"
+          >
+            Create new passkey
+          </Button>
+
+          {webauthnUI.signInError != null && (
+            <Alert variant="error">{webauthnUI.signInError}</Alert>
+          )}
+        </div>
+      )}
     </BaseModalDialog>
   )
 }
