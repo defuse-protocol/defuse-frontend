4806fc3e307992125d19ff8e37251131bd3577a8
jobotics <jo_botics@proton.me>
2026-01-26 14:10:17 +0000

    chore: add NonContactsHeader component

---
+++ src/components/DefuseSDK/features/contacts/NonContactsHeader.tsx
@@ -0,0 +1,9 @@
+const NonContactsHeader = () => {
+  return (
+    <h1 className="text-gray-900 text-xl font-semibold tracking-tight">
+      Contacts
+    </h1>
+  )
+}
+
+export default NonContactsHeader
