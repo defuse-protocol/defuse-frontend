8752d98cd92732aa2f9c4ae82e24dfac616341ee
santerisarle <santeri.sarle@gmail.com>
2026-02-03 09:36:40 +0300

    Wallet verify modal styling

--- src/components/DefuseSDK/components/Modal/ModalDialog/index.tsx
+++ src/components/DefuseSDK/components/Modal/ModalDialog/index.tsx
@@ -104,7 +104,7 @@ export function BaseModalDialog({
                 {status && (
                   <div
                     className={clsx(
-                      "absolute top-0 inset-x-0 h-32 bg-linear-to-b from-green-50 to-green-50/0",
+                      "absolute top-0 inset-x-0 h-32 bg-linear-to-b",
                       {
                         "from-green-50 to-green-50/0": status === "success",
                         "from-red-50 to-red-50/0": status === "error",
