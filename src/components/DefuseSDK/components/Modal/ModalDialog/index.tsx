3288a730b984e1d1394e06e97f7b76ef1b2608a2
santerisarle <santeri.sarle@gmail.com>
2026-02-05 20:00:10 +0300

    Fix modal alignment

--- src/components/DefuseSDK/components/Modal/ModalDialog/index.tsx
+++ src/components/DefuseSDK/components/Modal/ModalDialog/index.tsx
@@ -98,7 +98,7 @@ export function BaseModalDialog({
           <div
             role="presentation"
             className={clsx(
-              "flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0",
+              "flex min-h-full items-end justify-center p-4 text-center sm:items-start sm:p-0 sm:pt-[10vh]",
               !ignoreSidebar && "lg:pl-74"
             )}
             onClick={() => isDismissable && onClose?.()}
