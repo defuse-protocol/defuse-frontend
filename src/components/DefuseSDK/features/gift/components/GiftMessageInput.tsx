fde3bcb8e79c1d13ffe4fa603009215828be1eda
santerisarle <santeri.sarle@gmail.com>
2026-02-10 17:30:43 +0300

    Input to textarea

--- src/components/DefuseSDK/features/gift/components/GiftMessageInput.tsx
+++ src/components/DefuseSDK/features/gift/components/GiftMessageInput.tsx
@@ -1,8 +1,8 @@
 import { ChatBubbleBottomCenterTextIcon } from "@heroicons/react/20/solid"
-import type { InputHTMLAttributes } from "react"
+import type { TextareaHTMLAttributes } from "react"
 
 export function GiftMessageInput(
-  inputProps: InputHTMLAttributes<HTMLInputElement>
+  inputProps: TextareaHTMLAttributes<HTMLTextAreaElement>
 ) {
   const messageLength =
     typeof inputProps.value === "string" ? inputProps.value.length : 0
@@ -11,29 +11,31 @@ export function GiftMessageInput(
 
   return (
     <div className="mt-2 w-full">
-      <label className="flex items-center gap-3 rounded-3xl bg-white p-3 cursor-text outline-1 -outline-offset-1 focus-within:outline-2 focus-within:-outline-offset-2 outline-gray-200 focus-within:outline-gray-900">
-        <div className="bg-gray-100 rounded-full size-10 shrink-0 flex items-center justify-center">
-          <ChatBubbleBottomCenterTextIcon className="size-5 text-gray-500" />
+      <label className="flex gap-4 rounded-3xl bg-white p-3 cursor-text outline-1 -outline-offset-1 focus-within:outline-2 focus-within:-outline-offset-2 outline-gray-200 focus-within:outline-gray-900">
+        <div className="shrink-0">
+          <div className="bg-gray-100 rounded-full size-10 flex items-center justify-center">
+            <ChatBubbleBottomCenterTextIcon className="size-5 text-gray-500" />
+          </div>
+          {messageLength > 0 && (
+            <div className="mt-2 bg-gray-100 rounded-lg px-2 py-1 text-xs font-semibold text-gray-500 font-mono">
+              {remainingChars}
+            </div>
+          )}
         </div>
-        <div className="flex-1">
+        <div className="flex-1 min-h-10 pt-2.5">
           <span className="sr-only">Message</span>
-          <input
+          <textarea
             id="gift-message"
-            type="text"
             placeholder="Add a message (optional)"
             autoComplete="off"
             data-form-type="other"
             data-lpignore="true"
             data-1p-ignore="true"
-            className="block w-full text-gray-900 font-semibold placeholder:text-gray-400 focus:outline-none text-base leading-none ring-0 border-none p-0"
+            rows={5}
+            className="block w-full text-gray-900 font-semibold placeholder:text-gray-400 focus:outline-none text-base/5 ring-0 border-none p-0"
             {...inputProps}
           />
         </div>
-        {messageLength > 0 && (
-          <div className="shrink-0 bg-gray-100 rounded-lg px-2 py-1 text-xs font-semibold text-gray-500 font-mono">
-            {remainingChars}
-          </div>
-        )}
       </label>
     </div>
   )
