e7ead62914b7f19e33e15eed426d3ba004497b0a
devsparrow5 <devsparrow5@users.noreply.github.com>
2026-02-16 12:04:08 +0100

    Solve build issues

--- src/components/DefuseSDK/features/account/components/Balance.tsx
+++ src/components/DefuseSDK/features/account/components/Balance.tsx
@@ -6,15 +6,15 @@ import { FormattedCurrency } from "./shared/FormattedCurrency"
 const Balance = ({ balance }: { balance: number | undefined }) => (
   <section className="flex justify-between items-start">
     <div>
-      <h2 className="text-base text-fg-secondary font-medium">Account balance</h2>
+      <h2 className="text-base text-fg-secondary font-medium">
+        Account balance
+      </h2>
       <FormattedCurrency
         value={balance ?? 0}
         formatOptions={{ currency: "USD" }}
         className={clsx(
           "mt-2 font-bold text-5xl tracking-tight",
-          balance === undefined
-            ? "text-fg-tertiary animate-pulse"
-            : "text-fg"
+          balance === undefined ? "text-fg-tertiary animate-pulse" : "text-fg"
         )}
         centsClassName="text-3xl"
       />
